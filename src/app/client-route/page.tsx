f4b9e9583f61703cc4c9851198b5d812e56c0ffe
ammarAlzaqam <git@ammaralzaqam.com>
2025-07-05 21:17:37 +0300

    server-only code

---
+++ src/app/client-route/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { serverSideFunction } from "@/utils/server-utils";
+
+export default function ClientRoute() {
+//   const result = serverSideFunction();
+  return <h1>Client Route Page</h1>;
+}
