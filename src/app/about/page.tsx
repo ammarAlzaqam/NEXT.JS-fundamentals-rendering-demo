9b5b5a8f4fc86db3b30354698105a02cfa04c9cf
ammarAlzaqam <git@ammaralzaqam.com>
2025-07-05 10:32:37 +0300

    static rendering

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,10 +2,10 @@
 
 export default function About() {
   console.log("About page: server component by default");
-//   const [input, setInput] = useState(""); //! This React hook only works in a client component
+  //   const [input, setInput] = useState(""); //! This React hook only works in a client component
   return (
     <section>
-      <h1>About Page.</h1>
+      <h1>About Page. {new Date().toLocaleTimeString()}</h1>
     </section>
   );
 }
