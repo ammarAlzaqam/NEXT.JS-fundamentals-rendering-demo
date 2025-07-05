206a6ad51e876e841c148a11ce0681a325038656
ammarAlzaqam <git@ammaralzaqam.com>
2025-07-05 12:05:37 +0300

    dynamic params

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -2,6 +2,8 @@ interface ProductDetailsProps {
   params: Promise<{ id: string }>;
 }
 
+export const dynamicParams = false;
+
 export async function generateStaticParams() {
   return [{ id: "1" }, { id: "2" }, { id: "3" }];
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 
+
 export default function Products() {
   return (
     <section>
@@ -9,10 +10,10 @@ export default function Products() {
           <Link href="/products/1">product 1</Link>
         </li>
         <li>
-          <Link href="/products/1">product 2</Link>
+          <Link href="/products/2">product 2</Link>
         </li>
         <li>
-          <Link href="/products/1">product 3</Link>
+          <Link href="/products/3">product 3</Link>
         </li>
       </ul>
     </section>
